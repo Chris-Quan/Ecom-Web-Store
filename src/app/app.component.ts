173f8b40ce103fea86fd1f3e88d632084f10529d
Chris-Quan <chrisquan86@outlook.com>
2023-07-24 06:44:57 -0400

    changes to cart menu system

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { Cart } from './models/cart.model';
-import { CartService} from './services/cart.service'
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
-  <app-header [cart]="cart"></app-header>
+  <app-header></app-header>
   <router-outlet></router-outlet>
 
   `,
   styles: []
 })
-export class AppComponent implements OnInit {
-  cart: Cart = { items: [] };
-
-  constructor(private cartService: CartService) {}
-
-  ngOnInit() {
-    this.cartService.cart.subscribe((_cart) => {
-      this.cart = _cart;
-    });
-  }
+export class AppComponent {
+  title = 'store';
 }
