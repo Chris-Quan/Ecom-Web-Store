e642376bc0cc2e7623694f60115577f76d8d23dc
Chris-Quan <chrisquan86@outlook.com>
2023-07-25 08:24:01 -0400

    updated cart page

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -28,6 +28,27 @@ export class CartService {
   console.log(this.cart.value);
   }
 
+  removeQuantity(item: CartItem): void {
+    let itemForRemoval: CartItem | undefined;
+    let filteredItems = this.cart.value.items.map((_item) => {
+      if (_item.id === item.id){
+        _item.quantity--;
+
+        if (_item.quantity === 0){
+          itemForRemoval = _item
+        }
+      }
+
+      return _item
+    });
+    
+    if (itemForRemoval){
+      filteredItems = this.removeFromCart(itemForRemoval, false)
+    }
+    this.cart.next({items:filteredItems})
+    this._snackBar.open('Item removed from cart', 'ok', {duration:3000})
+  }
+
   getTotal(items: Array<CartItem>): number {
     return items.
       map((item) => item.price * item.quantity)
@@ -43,4 +64,16 @@ export class CartService {
 
   }
 
+  removeFromCart(item: CartItem, update = true): Array<CartItem>{
+    const filteredItems = this.cart.value.items.filter(
+      (_item) => _item.id !== item.id
+    )
+
+    if(update){
+      this.cart.next({items: filteredItems})
+      this._snackBar.open('Item removed from cart.', 'Ok', {duration: 3000});
+    }
+
+    return filteredItems
+  }
 }
