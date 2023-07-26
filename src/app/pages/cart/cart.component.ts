06b82450d91efdfdd09c32abcec2c45852ff8c4c
Chris-Quan <chrisquan86@outlook.com>
2023-07-26 12:12:06 -0400

    stripe api integration

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { loadStripe } from '@stripe/stripe-js';
 import { Cart, CartItem } from 'src/app/models/cart.model';
 import { CartService } from 'src/app/services/cart.service';
 
@@ -42,7 +44,7 @@ export class CartComponent {
     'action'
   ];
 
-  constructor(private cartService: CartService){}
+  constructor(private cartService: CartService, private http: HttpClient){}
 
   ngOnInit():void{
     this.cartService.cart.subscribe((_cart: Cart) =>{
@@ -75,5 +77,16 @@ export class CartComponent {
 
   onRemoveQuantity(item: CartItem):void{
     this.cartService.removeQuantity(item);
+  }
+
+  onCheckout(): void{
+    this.http.post('http://localhost:4242/checkout', {
+      items: this.cart.items
+    }).subscribe(async(res:any)=> {
+    let stripe = await loadStripe('pk_test_51NY6H5FeWoxjIutKaIZWqfSP4oRkSmrmDvG8UpEpPCp6GFDw10q5NrMAjRiY0NfaI5ehDLFhUk8aRupdIqYfgYAL00gyhkeIe7');
+    stripe?.redirectToCheckout({
+      sessionId: res.id
+    })
+  })
   }
 }
